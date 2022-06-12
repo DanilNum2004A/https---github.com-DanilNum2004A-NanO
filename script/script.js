f0e7093550d58c5ef8c1e4d9f67d5a47294dfc57
DanilNum2004A <dansel265@gmail.com>
2022-06-12 08:53:44 +0400

    DAD

---
+++ script/script.js
@@ -0,0 +1,20 @@
+document.addEventListener('click', e => {
+    const isDropdownButton = e.target.matches("[data-dropdown-button]")
+    if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
+
+    let currentDropdown
+    if (isDropdownButton) {
+        currentDropdown = e.target.closest('[data-dropdown]')
+        currentDropdown.classList.toggle('active')
+    }
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+        if (dropdown === currentDropdown) return
+        dropdown.classList.remove('active')
+    })
+})
+
+let nav = document.querySelector('#nav');
+
+document.querySelector('#mobile').onclick = () => {
+    nav.classList.toggle('show');
+}
